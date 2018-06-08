f295700cdf37ca9f5ec5592e110cd73ea2b17beb
Alexander Moller <amloelxer@gmail.com>
2018-06-08 11:15:06 -0400

    added button outlets

--- TS-Tac-Toe/ViewController.m
+++ TS-Tac-Toe/ViewController.m
@@ -16,13 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)singlePlayerButtonClicked:(UIButton *)sender {
+    
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)multiplayerButtonClicked:(UIButton *)sender {
+    
 }
 
 
