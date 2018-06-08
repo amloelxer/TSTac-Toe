7c33ad0f18ce655253f6c000cf541c76d0652e43
Alexander Moller <amloelxer@gmail.com>
2018-06-08 11:05:57 -0400

    rest of pod stuff

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform:ios,'11.0'
+use_frameworks!
+
+target 'TS-Tac-Toe' do 
+  pod 'RxSwift'
+  pod 'RxCocoa'
+end
---
+++ TS-Tac-Toe/TSTacToeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TSTacToeViewController.swift
+//  TS-Tac-Toe
+//
+//  Created by Alexander Moller on 6/8/18.
+//  Copyright © 2018 Talkspace. All rights reserved.
+//
+
+import UIKit
+
+class TSTacToeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
